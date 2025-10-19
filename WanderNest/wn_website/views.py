b7db403b607dac3bfe8c7a92c60544c461e18f18
Artem Skivko <skivko337@gmail.com>
2025-10-19 19:16:57 +0300

    add welcome page style moved to static files

--- WanderNest/wn_website/views.py
+++ WanderNest/wn_website/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_page(request):
-    return render(request,'templates.base.html')
+    return render(request,'wn_website/welcome_page.html')
